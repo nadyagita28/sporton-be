bfaf7d9a0dd365f75c13df3c33d719f97450d344
Nadya Gita Aprilya <nadyagita28@gmail.com>
2026-02-05 17:04:18 +0700

    session 8 finish

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import categoryRoutes from "./routes/category.routes";
 import productRoutes from "./routes/product.routes";
+import transactionRoutes from "./routes/transaction.routes";
+import bankRoutes from "./routes/bank.routes";
 import { authenticate } from "./middlewares/auth.middleware";
 import path from "path";
 
@@ -13,9 +15,11 @@ app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({limit: "10mb", extended: true}));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/banks", bankRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 app.get("/", (req, res) => {
     res.send("Sporton Backend API is Running");
